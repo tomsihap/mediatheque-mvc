dd4bbb5c285882063130270cf8d86a34eeb3b1e3
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2019-06-19 12:34:09 +0200

    Init project

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+$articles = [
+    'premier article',
+    'deuxieme article',
+    'blablabla'
+];
+
+var_dump($articles);
+
+dump($articles);
